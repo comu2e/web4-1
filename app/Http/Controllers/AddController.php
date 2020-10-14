5f7bd501f121062aad62c104e0209d59b5258dce
英士 <comu2e@gmail.com>
2020-10-14 18:27:22 +0900

    task3-4回答 radioボタンクリック時に、リンク先に飛ばす http://localhost:8000/index すべて http://localhost:8000/working　作業中 http://localhost:8000/done　完了

--- app/Http/Controllers/AddController.php
+++ app/Http/Controllers/AddController.php
@@ -8,41 +8,55 @@ use Illuminate\Http\Request;
 class AddController extends Controller
 {
 
-    public function add(Request $request){
+    public function index(Request $request)
+    {
         $tasks = Task::all();
-        return view('index',['items'=>$tasks]);
-
+        return view('index', ['items' => $tasks]);
     }
-    public function create(Request $request){
-        $this -> validate($request,Task::$rules);
+//    作業中の
+    public function working_index(Request $request)
+    {
+        $tasks = Task::where('status','作業中')->get();
+        return view('index', ['items' => $tasks]);
+    }
+    public function done_index(Request $request)
+    {
+        $tasks = Task::where('status','完了')->get();
+        return view('index', ['items' => $tasks]);
+    }
+
+    public function create(Request $request)
+    {
+        $this->validate($request, Task::$rules);
         $task = new Task;
-        $form = $request -> all();
+        $form = $request->all();
         unset($form['_token']);
-        $task -> status = '作業中';
-        $task -> fill($form)-> save();
+        $task->status = '作業中';
+        $task->fill($form)->save();
         return redirect('/index');
     }
 
 //作業中ボタンをクリックしたときのidのdatabaseを探してきて、indexページのitemsにわたす。
-    public function edit($id){
+    public function edit($id)
+    {
 
-        return view('index',['items'=>Task::find($id)]);
+        return view('index', ['items' => Task::find($id)]);
     }
     //作業中ボタンをクリックしたときのidのdatabaseを探してくる。
 //    ボタンクリックしたときのidからdatabaseを探して、$taskに代入
 //firstで単一レコードを取り出す。
 //単一レコードのstatusが作業中、完了に分けてupdateする。
 
-    public function update($id){
+    public function update($id)
+    {
 
-        $task = Task::where('id',$id);
+        $task = Task::where('id', $id);
         if ($task->first()->status === '作業中') {
             $task->update(['status' => '完了']);
-        }
-        elseif ($task->first()->status ==='完了') {
+        } elseif ($task->first()->status === '完了') {
             $task->update(['status' => '作業中']);
         }
-        return view('index',['items'=>Task::all()]);
+        return view('index', ['items' => Task::all()]);
 
     }
 
@@ -51,6 +65,31 @@ class AddController extends Controller
         $tasks = Task::find($id);
         $tasks->delete();
 
-        return redirect()->route('task.add');
+        return redirect()->route('index.add');
+    }
+
+    public function select()
+    {
+        $status_button = 'working';
+        if (!empty($status_button)) {
+
+        switch ($status_button){
+            case "all";
+                $task = Task::all();
+                return view('index', ['items' => $task]);
+            case 'working';
+                $task = Task::find('working')->all();
+                return view('index', ['items' => $task]);
+            case 'done';
+                $task = Task::find('done')->all();
+                return view('index', ['items' => $task]);
+            }
+        }
+        else{
+            $task = Task::all();
+            return view('index', ['items' => $task]);
+        }
     }
 }
+
+
